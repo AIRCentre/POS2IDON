2cc59e56eb901983718574d8c885549389f07b77
EmanuelCastanho <emanuel.castanho@aircentre.org>
2023-03-10 14:13:51 -0100

    Changed name to configs

--- Configs/User_Inputs.py
+++ Configs/User_Inputs.py
@@ -2,183 +2,184 @@
 # -*- coding: utf-8 -*-
 """
 User Inputs.
-Credentials must be modified in the hidden .env file due to security.
+Credentials must be modified in the hidden .env file.
 
 @author: AIR Centre
 """
 
-# WORKFLOW OPTIONS ----------------------------------------------------------------------------------------------------------------------------------------------------------
+# SEARCH PRODUCTS ##########################################################################
 
-# PERFORM SEARCH
-# The True option enables to query Sentinel-2 L1C Products in the Google Cloud Service catalogue or Copernicus Open Access Hub and produce a list of products saved in 'S2L1C_Products/S2L1CProducts_URLs.txt'
-# If the False option is used, this step will be ignored and the S2L1CProducts_URLs.txt should be placed in the S2L1CproductsFolder.
-# Boolean.
-Search = True
+# Query Sentinel-2 L1C Products in the services catalogue and creates a list of products 
+# that will be saved inside s2l1c_products_folder.
+# If False, this step will be ignored and the products list must exist inside s2l1c_products_folder
+# for the download to work.
+# Other inputs besides bool will stop the pré-start.
+search = True
 
-# PERFORM PROCESSING
-# The True option enables the downloading and processing steps of each Sentinel-2 L1C Products once at the time.
-# If the False option is used, all the following steps will be ignored.
-# Boolean.
-Processing = True
+# Search service:
+# "COAH" (Copernicus Open Access Hub - better for recent data and near real time application). 
+# "GC" (Google Cloud - better for old data and long term application).
+# "CDSE" (Copernicus Data Space Ecosystem) - NOT YET AVAILABLE.
+# Other inputs besides string will stop the pré-start.
+# Other strings will be considered as "CDSE".
+service = "GC"
 
-# PERFORM DOWNLOAD
-# The True option enables to Download Sentinel-2 L1C Products from the Google Cloud Service or Copernicus Open Access Hub.
-# If the False option is used, this step will be ignored and the S2L1CproductsFolder must contain data.
-# Boolean.
-Download = True
+# Region Of Interest (ROI): 
+# SentinelHub EOBrowser (https://apps.sentinel-hub.com/eo-browser/) format. 
+# Also used by ACOLITE. If ROI has limits outside the product, ACOLITE will ignore.
+# Other inputs besides dictionary will stop the pré-start.
+roi = {"type":"Polygon","coordinates":[[[-88.308792,15.660726],[-88.308792,15.928978],[-88.040314,15.928978],[-88.040314,15.660726],[-88.308792,15.660726]]]}
 
-# PERFORM ATMOSPHERIC CORRECTION
-# The True option enables to AC Sentinel-2 L1C Products. 
-# If the False option is used, this step will be ignored and ACOLITEoutputsFolder must contain data.
-# Boolean.
-AtmosCorrect = True
+# Near real time sensing period:
+# Uses yesterday as start date and today as end date. 
+# sensing_period is ignored.
+# Other inputs besides bool will stop the pré-start.
+nrt_sensing_period = False
 
-# CALCULTE INDICES FROM NOT MASKED ACOLITE BANDS
-# The True option enables to calculate indices from acolite bands (not masked). 
-# If the False option is used, this step will be ignored.
-# Boolean.
-CalculateNotMaskedIndices = True
+# Sensing period of interest:
+# Used only if nrt_sensing_period = False.
+# (StartDate, EndDate).
+# Other inputs besides tuple will stop the pré-start.
+sensing_period = ('20200918', '20200918')
 
-# PERFORM MIDDLE STEPS
-# The True option enables to create masks, apply masks and calculate indices for Sentinel-2 images. 
-# If the False option is used, this step will be ignored and MaskingProduct Folder & MaskedProduct Folder must contain data.
-# Boolean.
-MiddleSteps = True
+# STREAM PROCESSING ########################################################################
 
-# PERFORM CLASSIFICATION
-# The True option enables to perform the classification of Sentinel-2 images. 
-# If the False option is used, this step will be ignored and SC map & PROBA map will not be created.
-# Boolean.
-Classify = True
-
-# STORAGE OPTIONS ----------------------------------------------------------------------------------------------------------------------------------------------------------
-
-# DELETE NOT NECESSARY INTERMEDIATED FILES
-# The True option enables, to delete all ACOLITE outputs (folders are kept) and masked bands and indices in the Results_MaskedBands-Indices-SCmaps folder (SCmap and PROBAmap are kept).
-# If the False option is used, this step will be ignored and the file is stored in the S2L1C Product Folder.
-# Boolean.
-DeleteIntermediateFiles = False
-
-# DELETE ORIGINAL PRODUCTS
-# The True option enables to delete the SAFE product folder after the image is processed. 
-# If the False option is used, this step will be ignored and the file is stored in the S2L1C Product Folder.
-# Boolean.
-DeleteOriginalProduct = False
+# Includes stream download, atmospheric correction, masks and classification.
+# URLs file created by SEARCH PRODUCTS must exist.
+# You can also create a dummy URLs list file named S2L1CProducts_URLs.txt, inside place dummy
+# URLs: dummy/NAMEOFPRODUCT1.SAFE
+#       dummy/NAMEOFPRODUCT2.SAFE
+# The STREAM PROCESSING needs to have S2L1C products that correspond to the URLs file, 
+# even if you have already atmospheric corrected products.
+# Other inputs besides bool will stop the pré-start.
+stream_processing = True
 
 
-# SEARCH AND DOWNLOAD OPTIONS --------------------------------------------------------------------------------------------------------------------------------------------------
+# Download is done in stream fashion. Use SEARCH if needed.
+# Download Sentinel-2 L1C Products from the GC or COAH services using URLs file. 
+# Other inputs besides bool will stop the pré-start.
+download = True
 
-# REGION OF INTEREST 
-# SentinelHub EOBrowser (https://apps.sentinel-hub.com/eo-browser/) format. If ROI has limits outside the product, ACOLITE will ignore.
-# Dictionary.
-# Example: ROI = {"type":"Polygon","coordinates":[[[-88.308792,15.660726],[-88.308792,15.928978],[-88.040314,15.928978],[-88.040314,15.660726],[-88.308792,15.660726]]]}
-ROI = {"type":"Polygon","coordinates":[[[-88.308792,15.660726],[-88.308792,15.928978],[-88.040314,15.928978],[-88.040314,15.660726],[-88.308792,15.660726]]]}
-
-# SENSING PERIOD OF INTEREST
-# (StartDate, EndDate). Needed if StartDateYesterday = False. 
-# Tuple of strings as ('YYYYMMDD','YYYYMMDD').
-# Example: SensingPeriod = ('20200918', '20200918')
-SensingPeriod = ('20200918', '20200918')
-
-# RECENT SENSING PERIOD
-# Use yesterday as start date and today as end date. SensingPeriod is ignored. Google Cloud download service is ignored.
-# Boolean.
-StartDateYesterday = False
-
-# DOWNLOAD SERVICE 
-# COAH (Copernicus Open Access Hub - better for recent data and near real time application) or GC (Google Cloud - better for old data and long term application)
-# String: "COAH" or "GC"
-DownloadService = "GC"
+# Download options.
+# Other inputs besides dictionary with right inputs will stop the pré-start.
+                    # Number of Long Term Archive attempts for COAH service.
+                    # Minimum is 1 attempt. Script waits 60 seconds between each attempt.
+download_options = {"lta_attempts": 2}  
 
 
-# MASK OPTIONS ----------------------------------------------------------------------------------------------------------------------------------------------------------
-
-# ESA WORLDCOVER
-# Use existant ESA WorldCover Tiles that are inside ESAwcFolder to create water mask. The downloading is ignored.
-# Boolean.
-UseExistantESAwc = False
-
-# LAND BUFFER SIZE
-# Buffer size applied to land, if 0 is used the buffer step is ignored.
-# Integer.
-BufferSize = 0
-
-# NDWI or BAND 8 MASKING
-# Create NDWI or BAND8 based mask. 
-# Select type of masking. 'NDWI' , 'BAND8' , None 
-Select_NDWI_or_BAND8_Masking = 'BAND8'
-# NDWI threshold and dilation (number of iteration must be equal or greater then 1) value to create mask
-NDWIthreshold=0.5 ; NDWIDilation_Size=6
-# Band 8 threshold and dilation (number of iteration must be equal or greater then 1) value to create mask
-Band8threshold=0.01 ; Band8Dilation_Size=2
-
-# CLOUDS
-# Create cloud mask using s2cloudless. 
-# Boolean.
-CloudMaskingS2cloudless = True
-# Specify the cloud probability threshold. pixels with cloud probability above this threshold are masked as cloudy pixels. 
-S2CL_Threshold=0.4 
-# Size of the disk in pixels for performing convolution (averaging probability over pixels).
-S2CL_Average=2
-#Size of the disk in pixels for performing dilation.
-S2CL_Dilation=1
-
-# NEGATIVE REFLECTANCES
-# Remove pixels with negative reflectances from masked bands. 
-# Boolean.
-RemoveNegativeReflectances = True
+# Atmospheric correction of Sentinel-2 L1C Products using ACOLITE. 
+# s2l1c_products_folder must exist with data.
+# Other inputs besides bool will stop the pré-start.
+atmospheric_correction = True
 
 
-# CLASSIFICATION -------------------------------------------------------------------------------------------------------------------------------------------------------
+# Apply masks to the atmospheric corrected product.
+# ac_products_folder must exist and have data.
+# Other inputs besides bool will stop the pré-start.
+masking = True
 
-# MODEL FOLDER LOCATION
-# Location of the scikit model (.pkl) and pytorch conversion (.zip) or julia model
-# The name of the model will be used to select the features (bands and indices) during classification.
-# String.
-#ModelFolderPath = "Configs/MLmodels/YourModelFolder"
-ModelFolderPath = "Configs/MLmodels/RF_Model_Example_MARIDA"
+# Masking options.
+# Other inputs besides dictionary with right inputs will stop the pré-start.
+                   # Use existing ESA WorldCover Tiles that are inside 2-1_ESA_Worldcover to create water mask.
+                   # If False, it will download the tiles.
+                   # Ignore water mask by using option True without tiles in the 2-1_ESA_Worldcover folder.
+masking_options = {"use_existing_ESAwc": False,  
+                   # Buffer size applied to land, 0 to ignore.
+                   "land_buffer": 0,
+                   # Apply mask based on 'NDWI' or 'BAND8' (features), None to ignore.
+                   "features_mask": 'BAND8',
+                   # NDWI and Band8 thresholds
+                   "threshold_values": [0.5, 0.01],
+                   # NDWI and Band8 dilations (number of iteration must be equal or greater then 1)
+                   "dilation_values": [6, 2],
+                   # Create cloud mask using s2cloudless, false to ignore.
+                   "cloud_mask": True,
+                   # Cloud probability threshold, pixels with cloud probability above this threshold are masked as cloudy pixels.
+                   "cloud_mask_threshold": 0.4,
+                   # Size of the disk in pixels to performe convolution (averaging probability over pixels).
+                   "cloud_mask_average": 2,
+                   # Size of the disk in pixels to performe dilation.
+                   "cloud_mask_dilation": 1
+                   }
 
-# Machine Learning Algorithm TYPE (only for python models)
-# Specify if the model is "rf" for random forest | "xgb" for xgboost
-MLAlgorithm = "rf"
 
-# MODEL TYPE (only for python models)
-# CPUpt for model converted to pytorch and processed on CPU.
-# GPUpt for model converted to pytorch and processed on GPU. Max size of model limited by GPU memory. Doesnt work on macOS. 
-# sk for original model created with scikit, other inputs will be consider as sk.
-# String.
-ModelType = "sk"
+# Perform classification on masked products.
+# masked_products_folder must exist and have data.
+# Other inputs besides bool will stop the pré-start.
+classification = True
 
-# CLASS PROBABILITIES (only for python)
-# Outputs .tif file with the class probability for each pixel predicted by the model
-# Boolean.
-ClassProba = True
+# Classification options.
+# Other inputs besides dictionary with right inputs will stop the pré-start.                       
+                          # Split full image into 256x256 patches and consider each one during classification.
+                          # Mosaic all patches into single image after classification.
+classification_options = {"split_and_mosaic": False,
+                          # Outputs TIF file with the class probability for each pixel.
+                          "classification_probabilities": False,
+                          # Machine Learning algorithm:
+                          # "rf" for Random Forest. 
+                          # "xgb" for XGBoost. 
+                          # "unet" for Unet. This model needs split_and_mosaic option True.
+                          "ml_algorithm": "rf",
+                          # Path to the folder containing the machine learning model.
+                          "model_path": "configs/MLmodels/RF_Model_Example_MARIDA",
+                          # Model_type:
+                          # "sk" for original model created with scikit.
+                          # "CPUpt" for model converted to pytorch and processed on CPU.
+                          # "GPUpt" for model converted to pytorch and processed on GPU. Max size of model limited by GPU memory.
+                          # None for Unet models.
+                          "model_type": "sk",
+                          # Number of classification classes. 6 or 11
+                          "n_classes": 11,
+                          # Tuple of features to consider, must match the ones used during model train.
+                          # Features available are ('B01', 'B02', 'B03', 'B04', 'B05', 'B06', 'B07', 'B08', 'B8A', 'B11', 'B12', 'NDVI', 'FAI', 'FDI', 'SI', 'NDWI', 'NRD', 'NDMI', 'BSI') or ('B01', 'B02', 'B03', 'B04', 'B05', 'B06', 'B07', 'B08', 'B8A', 'B11', 'B12', 'NDVI', 'FDI', 'NDWI', 'NDMI')
+                          "features": ('B01', 'B02', 'B03', 'B04', 'B05', 'B06', 'B07', 'B08', 'B8A', 'B11', 'B12', 'NDVI', 'FAI', 'FDI', 'SI', 'NDWI', 'NRD', 'NDMI', 'BSI'),
+                          # Only used for Unet models. Number of hidden channels.
+                          "n_hchannels": 16,
+                          # Only used for Unet models. Mean value for each feature.
+                          "features_mean": [0.05197577, 0.04783991, 0.04056812, 0.03163572, 0.02972606, 0.03457443, 0.03875053, 0.03436435, 0.0392113,  0.02358126, 0.01588816],
+                          # Only used for Unet models. Standard Deviation value for each feature.
+                          "features_std": [0.04725893, 0.04743808, 0.04699043, 0.04967381, 0.04946782, 0.06458357, 0.07594915, 0.07120246, 0.08251058, 0.05111466, 0.03524419]
+                          }
 
-# PERFORM CLASSIFICATION IN JULIA (test-phase)
-# Boolean.
-ClassifyinJulia = False
-# Indicate Julia model name
-RF_model_jl = "YourModel.jld2"
 
-# FOLDERS NAMES -----------------------------------------------------------------------------------------------------------------------------------------------------
+# Delete processed folders and files:
+# Other inputs besides dictionary with right inputs will stop the pré-start.
+          # Delete original products:
+          # Deletes original product after each processing.
+delete = {"original_products": False,
+          # Delete some intermediate after each processing - Recommended:
+          # Deletes Surface_Reflectance_Bands, Top_Atmosphere_Bands, masked Patches,
+          # Mosaics and single intermediate files in both sc_maps and proba_maps.
+          # But DOESN'T, delete atmospheric correction stack, Masks, masked stack.
+          "some_intermediate": False,
+          # Delete all intermediate after each processing - Not Recommended:
+          # Only final results available.
+          # Deletes Surface_Reflectance_Bands, Top_Atmosphere_Bands, masked Patches,
+          # Mosaics and single intermediate files in both sc_maps and proba_maps.
+          # BUT ALSO, deletes atmospheric correction stack, Masks, masked stack.
+          "all_intermediate": False
+          }
 
-# Folder where the downloaded S2-L1C products will be saved. 
-# String.
-S2L1CproductsFolder = 'S2L1C_Products'
 
+# FOLDERS NAMES ############################################################################
+
+# Download folder:
+# Folder where the URLs file and downloaded S2L1C products will be saved.
+# Other inputs besides string will stop the pré-start.
+s2l1c_products_folder = "0_S2L1C_Products"
+
+# Atmospheric correction folder:
 # Folder where the S2 atmospherically corrected bands will be saved. 
-# String.
-ACOLITEoutputsFolder = 'ACOLITE_Outputs'
+# Other inputs besides string will stop the pré-start.
+ac_products_folder = "1_Atmospheric_Corrected_Products"
 
-# Folder where the ESA WorldCover maps will be saved.
-# String.
-ESAwcFolder = 'ESAwc_Tiles'
+# Masking folder:
+# Folder where the masked products will be saved. The water, features and cloud masks will also
+# be saved in this folder.
+# Other inputs besides string will stop the pré-start.
+masked_products_folder = "2_Masked_Products"
 
-# Folder where the masking will be saved. 
-# String.
-MaskingProductsFolder = 'Masking_Outputs'
-MaskedProductsFolder = 'Masked_Outputs'
-
-# Folder where the final results (masked bands, indices and scene classification map) will be saved. 
-# String.
-ResultsFolder = 'Results_SCmaps'
+# Classification folder:
+# Folder where the final classification results will be saved.
+# Other inputs besides string will stop the pré-start.
+classification_products_folder = "3_Classification_Results"
